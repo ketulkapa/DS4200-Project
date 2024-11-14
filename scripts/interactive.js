fd448c7450eb72a9172e2c722f7489b24979c431
ketulkapa <kapadia.ket@northeastern.edu>
2024-11-14 16:08:31 -0500

    fixed the animation for loading and organized the scripting file

---
+++ scripts/interactive.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function () {
+    document.getElementById("loading-indicator").style.display = "none";
+    document.getElementById("loading-bar-container").style.display = "none";
+});
